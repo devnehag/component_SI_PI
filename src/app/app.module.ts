c8d34dc7945bf953072353e8259985c41b1c36dc
Neha Gupta <gupta.neha2004@gmail.com>
2023-11-26 05:36:02 -0800

    Components, Nested Comp, Data(SI) Property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AlertComponent } from './alert/alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AlertComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
