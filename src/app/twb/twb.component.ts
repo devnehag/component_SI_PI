2023bcb05931ded6d9a2d1f7ec6ebc10bcc0794a
Neha Gupta <gupta.neha2004@gmail.com>
2023-11-27 10:51:08 -0800

    Two Way data binding

---
+++ src/app/twb/twb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-twb',
+  templateUrl: './twb.component.html',
+  styleUrls: ['./twb.component.css']
+})
+export class TwbComponent {
+  title:string ='This is my title';
+}
